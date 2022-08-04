a64a8c59d73b644ff1571ff0e3f0a9a5bb5e2c82
shtarek2606 <102531870+shtarek2606@users.noreply.github.com>
2022-08-04 10:16:51 +0200

    Update PersonService.java

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -40,6 +40,13 @@ public class PersonService {
 		p.setLastName("Mahmoud");
 		persons.add(p);
 		
+		p = new Person();
+		p.setId("5");
+		p.setAge(27);
+		p.setFirstName("sherif");
+		p.setLastName("Tarek");
+		persons.add(p);
+		
 	}
 	public Person getPerson(String id) {
 		for(Person person:persons) {
